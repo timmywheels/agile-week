aa6a2b1904e2206a3c6a0715e70054316b129954
Tim Wheeler <tim@timwheeler.com>
2018-06-22 21:38:41 +0000

    Fixed search filter to search only #client-list-ul>li

--- js/app.js
+++ js/app.js
@@ -182,7 +182,7 @@ document.addEventListener('DOMContentLoaded', function() {
   const searchBar = document.forms['search-clients'].querySelector('input');
   searchBar.addEventListener('keyup', function(e) {
     const term = e.target.value.toLowerCase();
-    const clients = list.getElementsByTagName('li');
+    const clients = $('#client-list-ul>li');
 
     Array.from(clients).forEach(function(client) {
       const title = client.innerText;
